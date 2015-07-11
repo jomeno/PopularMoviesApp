b7b9ec7b1c777d4fb15f8448e9af433ba0ffe5d0
Jomeno <jomeno.vona@gmail.com>
2015-07-11 06:24:53 +0100

    Initial Commit

---
+++ PopularMoviesApp/app/src/main/java/com/example/android/popularmoviesapp/adapter/MoviesAdapter.java
@@ -0,0 +1,18 @@
+package com.example.android.popularmoviesapp.adapter;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Jomeno on 7/11/2015.
+ */
+public class MoviesAdapter extends AsyncTask<Void, Void, Void> {
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        return null;
+    }
+}
